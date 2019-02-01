aa9e79acfdbd2dbb1c92692f9609dafaed73462e
Kenneth M Doud <kmdoud@gmail.com>
2019-02-01 15:25:20 -0500

    Initial Commit

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,13 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            var pgm = new ArrayOfIntegers();
+            pgm.Add(0, 42);
+            pgm.Add(1, 25);
+            pgm.Add(2, 97);
+            pgm.Print();
+
+
         }
     }
 }
